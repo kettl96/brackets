d0c32fdacea46257b375c44747be68a0e52b9e24
kettl96 <kettl110e4@gmail.com>
2022-01-20 14:47:20 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = bracketsConfig.map(pair => pair.join(''))
+  for (let i = 0; i < brackets.length;) {
+    if (str.includes(brackets[i])) {
+      str = str.replace(brackets[i], '')
+      i = 0
+    }
+    else i++
+  }
+  return str === ''
 }
